5e9c92762f801a1b4faf91f7806e19f526f07a93
Arnoud Sietsema <arnoud@ecedo.com>
2017-09-27 15:44:33 +0200

    fix voor mock param

--- src/main/java/nl/getthere/robo/RoboJar.java
+++ src/main/java/nl/getthere/robo/RoboJar.java
@@ -39,7 +39,7 @@ public class RoboJar {
 	}
 
 	public static void main(String[] args) {
-		new RoboJar(args.length > 0).go();
+		new RoboJar(args.length > 0 && args[0].equals("mock")).go();
 	}
 
 	//Getters
